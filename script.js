baf10d8770220595e047443119a41fd0950dd59a
viswanathamsantosh <viswanathamsantosh@gmail.com>
2017-01-27 13:55:31 +0530

    Created a new branch for version 1.0.0

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ for (var i = 0; i < len; i++){
 	links[i].firstChild.style.color="white";
 }
 
-var spanTags=doc.getElementsByTagName("span");
-var spanLen=spanTags.length;
-for (var i = 0; i < spanLen; i++){
-	if(spanTags[i].getAttribute("itemprop")=="headline")
-		spanTags[i].style.color="white";
-}
+// var spanTags=doc.getElementsByTagName("span");
+// var spanLen=spanTags.length;
+// for (var i = 0; i < spanLen; i++){
+// 	if(spanTags[i].getAttribute("itemprop")=="headline")
+// 		spanTags[i].style.color="white";
+// }
 
-var divTags=doc.getElementsByTagName("div");
-var divLen=divTags.length;
-for (var i = 0; i < divLen; i++){
-	if(divTags[i].getAttribute("itemprop")=="articleBody")
-		divTags[i].style.color="white";
-}
+// var divTags=doc.getElementsByTagName("div");
+// var divLen=divTags.length;
+// for (var i = 0; i < divLen; i++){
+// 	if(divTags[i].getAttribute("itemprop")=="articleBody")
+// 		divTags[i].style.color="white";
+// }
 
 var loadMoreButton=doc.getElementById("load-more-btn");
 loadMoreButton.style.backgroundColor="red";
