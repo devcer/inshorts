f74bf8160c9adb7ca6324b6a2318500df0c92b5b
viswanathamsantosh <viswanathamsantosh@gmail.com>
2017-02-11 16:34:35 +0530

    I think its ready

--- background.js
+++ background.js
@@ -1,14 +1,35 @@
+var CSS='body{background-color: black;}span[itemprop="headline"]{color:white;}div[itemprop="articleBody"]{color:white;} #load-more-btn{background-color:red;color:white;}'
+var enable=true;
+//method to enable nightmode
+function enableNightMode(tab) {
+	console.log("enableNightMode");
+	browser.tabs.insertCSS({code: CSS});
+	enable=false;
+}
 
+//method to disable nightmode
+function disableNightMode(tab){
+	console.log("disableNightMode");
+	browser.tabs.removeCSS({code: CSS});
+	enable=true;
+}
 
-// function initializePageAction(tab) {
-//     browser.pageAction.show(tab.id);
-// }
+//method to toggle mode
+function toggleNightMode(tab){
+	console.log("toggleNightMode");
+	if(tab.url.indexOf("www.inshorts.com/en/read")!=-1){
+		if(enable){
+			enableNightMode(tab);
+		}else{
+			disableNightMode(tab);
+		}
+	}
+}
 
-// var gettingAllTabs = browser.tabs.query({});
-// gettingAllTabs.then((tabs) => {
-//   for (tab of tabs) {
-//     initializePageAction(tab);
-//   }
-// });
+browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+	if(tab.url.indexOf("www.inshorts.com/en/read")!=-1){
+		disableNightMode(tab);
+	}
+});
 
-//browser.browserAction.onClicked.addListener(changeBackground);
+browser.browserAction.onClicked.addListener(toggleNightMode);
