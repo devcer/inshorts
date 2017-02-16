0f488ad0af29ed3a25f5584357ace7917b4bcf2d
viswanathamsantosh <viswanathamsantosh@gmail.com>
2017-02-16 23:54:10 +0530

    removed unnecessary code

--- background.js
+++ background.js
@@ -1,34 +1,31 @@
 var CSS='body{background-color: black;}span[itemprop="headline"]{color:white;}div[itemprop="articleBody"]{color:white;} #load-more-btn{background-color:red;color:white;}'
 var enable=true;
 //method to enable nightmode
-function enableNightMode(tab) {
-	console.log("enableNightMode");
+function enableNightMode() {
 	browser.tabs.insertCSS({code: CSS});
 	enable=false;
 }
 
 //method to disable nightmode
-function disableNightMode(tab){
-	console.log("disableNightMode");
+function disableNightMode(){
 	browser.tabs.removeCSS({code: CSS});
 	enable=true;
 }
 
 //method to toggle mode
 function toggleNightMode(tab){
-	console.log("toggleNightMode");
 	if(tab.url.indexOf("www.inshorts.com/en/read")!=-1){
 		if(enable){
-			enableNightMode(tab);
+			enableNightMode();
 		}else{
-			disableNightMode(tab);
+			disableNightMode();
 		}
 	}
 }
 
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
 	if(tab.url.indexOf("www.inshorts.com/en/read")!=-1){
-		disableNightMode(tab);
+		disableNightMode();
 	}
 });
 
